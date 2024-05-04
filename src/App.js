b33c83c98bbdf2d95637859dd61e34263966e809
PTACE1\trung <mannguyensp@gmail.com>
2024-05-04 18:15:23 +0700

    add commit

--- src/App.js
+++ src/App.js
@@ -3,14 +3,14 @@ import Header from "./conponents/header/Header";
 import Home from "./conponents/home/Home";
 
 function App() {
-    return (
-        <>
-            <Header />
-            <main>
-                <Home />
-            </main>
-        </>
-    );
+  return (
+    <>
+      <Header />
+      <main>
+        <Home />
+      </main>
+    </>
+  );
 }
 
 export default App;
--- src/conponents/home/Home.jsx
+++ src/conponents/home/Home.jsx
@@ -5,20 +5,20 @@ import Data from "./Data";
 import ScrollDown from "./ScrollDown";
 
 const Home = () => {
-    return (
-        <section className="home section" id="home">
-            <div className="home__container container grid">
-                <div className="home__content grid">
-                    <Social />
+  return (
+    <section className="home section" id="home">
+      <div className="home__container container grid">
+        <div className="home__content grid">
+          <Social />
 
-                    <div className="home__img"></div>
+          <div className="home__img"></div>
 
-                    <Data />
-                </div>
-                <ScrollDown />
-            </div>
-        </section>
-    );
+          <Data />
+        </div>
+        <ScrollDown />
+      </div>
+    </section>
+  );
 };
 
 export default Home;
