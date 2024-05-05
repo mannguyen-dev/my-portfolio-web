aca4f3e54921770948138388a1da9ec9c8c8b832
PTACE1\trung <mannguyensp@gmail.com>
2024-05-05 20:53:51 +0700

    Complete develope

--- src/App.js
+++ src/App.js
@@ -1,16 +1,35 @@
+import React from "react";
 import "./App.css";
 import Header from "./conponents/header/Header";
 import Home from "./conponents/home/Home";
+import About from "./conponents/about/About";
+import Skills from "./conponents/skills/Skills";
+import Services from "./conponents/services/Services";
+import Qualification from "./conponents/qualification/Qualification";
+import Testimonials from "./conponents/testimonials/Testimonials";
+import Contact from "./conponents/contact/Contact";
+import Footer from "./conponents/footer/Footer";
+import ScrollUp from "./conponents/scrollup/Scrollup";
 
-function App() {
+const App = () => {
   return (
     <>
       <Header />
-      <main>
+
+      <main className="main">
         <Home />
+        <About />
+        <Skills />
+        <Services />
+        <Qualification />
+        <Testimonials />
+        <Contact />
       </main>
+
+      <Footer />
+      <ScrollUp />
     </>
   );
-}
+};
 
 export default App;
--- src/conponents/home/Data.jsx
+++ src/conponents/home/Data.jsx
@@ -55,7 +55,6 @@ const Data = () => {
           ></path>
         </svg>
       </h1>
-
       <h3 className="home__subtitle">Visual Designer</h3>
       <p className="home__description">
         I'm creative designer based in New York, and I'm very passionate and
